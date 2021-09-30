fe9574151cf788f7c4825a824126b14897caacdf
Keycatowo <owo.nthu@outlook.com>
2021-09-30 16:31:48 +0800

    add-修改README到pypi上

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="svo-extract",
-    version="0.0.1",
+    version="0.0.2",
     author="owo",
     author_email="key.cat@outlook.com",
     description="A package using clip to extrac S,V,O",
